a12b54534c278726e598a7f07e4d5b6cc92868ac
Vincent <VincentHeemskerk@users.noreply.github.com>
2020-05-27 11:31:07 +0200

    update small issues
    update header image
    update css for header image
    update json files

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // constants
-// const jsonFile = 'vogels.json';
-// const jsonFile = '8448-Birds-of-Netherlands.taxonomic.json';
 const jsonFile = 'largelist.json';
 
 const addOverview = (element) => {
@@ -17,82 +15,43 @@ const addChecklist = () => {
 const addTable = () => {
     readJSON(jsonFile, function(text) {
         let data = JSON.parse(text);
-        // createTable(data);
         createTableLarge(data);
     }); 
 }
 
-const createHeaderRow = (row, obj) => {
-    const cell = row.c
-}
+// const createHeaderRow = (row, obj) => {
+//     const cell = row.c
+// }
 
 const createTableLarge = arr => {
     const table = document.createElement('table');
-    // const firstRow = table.insertRow();
     arr.forEach(element => {
-        const row = table.insertRow();
+        const row = table.insertRow();  
+        // fill other cells
         Object.keys(element).forEach(function(key, index) {
             const cell = row.insertCell();
-            cell.innerHTML = element[key];
+            switch(key) {
+                case 'spotted':
+                    const checkCell = row.insertCell();
+                    const checkbox = document.createElement('input');
+                    checkbox.type = 'checkbox';
+                    checkbox.value = false;
+                    checkbox.classList.add('js-checkbox');
+                    checkCell.appendChild(checkbox);
+                    break;
+                case 'url':
+                    console.log('url');
+                    const link = document.createElement('a');
+                    link.href = element[key];
+                    link.innerHTML = 'klik voor foto en info';
+                    cell.appendChild(link);
+                    break;
+                default:
+                    cell.innerHTML = element[key];
+            };
         });      
     });   
     addOverview(table);
-
-}
-
-const createTable = arr => {
-    const table = document.createElement('table');
-    const firstRow = table.insertRow();
-    for(let i = 0; i < Object.keys(arr.vogellijst.vogels[0]).length; i++) {
-        const cell = firstRow.insertCell();
-        cell.innerHTML = Object.keys(arr.vogellijst.vogels[0])[i];
-    }
-    arr.vogellijst.vogels.forEach(vogel => {
-        const row = table.insertRow();
-        Object.keys(vogel).forEach(function(key, index) {
-            const cell = row.insertCell();
-            if (key === 'gespot') {
-                const checkbox = document.createElement('input');
-                checkbox.type = 'checkbox';
-                checkbox.value = vogel[key];
-                checkbox.classList.add('js-checkbox');
-                if (vogel[key] === true) {
-                    checkbox.classList.add('isSpotted');
-                }
-                checkbox.addEventListener('change', function() {
-                    this.value = this.checked ? true : false;
-                });
-                cell.appendChild(checkbox);
-            }
-            else {
-                cell.innerHTML = vogel[key];
-            }
-        });
-        row.classList.add(vogel.foto);
-        row.addEventListener('click', function(e) {
-            // show modal with picture
-            const image = document.createElement('img');
-            image.src = './img/'+vogel.foto;
-            image.alt = vogel.foto;
-            image.classList.add('birdImage');
-            document.getElementById('checklist').appendChild(image);
-        })
-    });
-    addOverview(table);
-}
-
-const createList = arr => {
-    const list = document.createElement("ul");
-    arr.vogellijst.vogels.forEach(vogel => {
-        const liItem = document.createElement("li");
-        const liText = document.createTextNode(addItem(vogel));
-        liItem.appendChild(liText);
-        list.appendChild(liItem);
-    });
-    addOverview.appendChild(list);
-}
-const addItem = (item) => {
-    return item.naam;
 }
 
 const readJSON = (file, callback) => {
